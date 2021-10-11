61e5cc08987ae7b42f4beabd3cd5265aebd39ed8
xypherDev <abdullahqureshi4444@gmail.com>
2021-10-11 21:32:08 +0500

    Day-4 Completed

--- lib/signin_screen.dart
+++ lib/signin_screen.dart
@@ -22,34 +22,116 @@ class SignInScreen extends StatelessWidget {
             ),
           ),
           Expanded(
-            flex: 3,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('SIGN IN', style: Theme.of(context).textTheme.headline4,),
-                      Text('SIGN UP', style: Theme.of(context).textTheme.button,),
-                    ],
-                  ),
-                ),
-                Row(
-                  children: const <Widget>[
-                     Icon(
-                      Icons.alternate_email,
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(
+                children: [
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom : 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('SIGN IN', style: Theme.of(context).textTheme.headline4,),
+                        Text('SIGN UP', style: Theme.of(context).textTheme.button?.copyWith(color : kPrimaryColor),),
+                      ],
                     ),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Email Address'
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom : 20.0),
+                    child: Row(
+                      children: const <Widget>[
+                         Padding(
+                           padding: EdgeInsets.only(right : 16.0),
+                           child: Icon(
+                            Icons.alternate_email,
+                             color: kPrimaryColor,
                         ),
-                      ),
-                    )
-                  ],
-                )
-              ],
+                         ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Email Address',
+                              hintStyle: TextStyle(color: kPrimaryColor),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom : 20.0),
+                    child: Row(
+                      children: const <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(right : 16.0),
+                          child: Icon(
+                            Icons.lock,
+                            color: kPrimaryColor,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              hintStyle: TextStyle(color: kPrimaryColor),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom : 30.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.1),
+                            //shape: BoxShape.circle,
+                            borderRadius: const BorderRadius.all(Radius.circular(40))
+                          ),
+                          child: Icon(
+                            Icons.android_rounded,
+                            color: Colors.white.withOpacity(0.3),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                              color: Colors.white.withOpacity(0.1),
+                              //shape: BoxShape.circle,
+                              borderRadius: const BorderRadius.all(Radius.circular(40))
+                          ),
+                          child: Icon(
+                            Icons.chat,
+                            color: Colors.white.withOpacity(0.3),
+                          ),
+                        ),
+                        const Spacer(),
+                        Container(
+                          padding: const EdgeInsets.all(15),
+                          decoration: const BoxDecoration(
+                              color: kPrimaryColor,
+                              //shape: BoxShape.circle,
+                              borderRadius: BorderRadius.all(Radius.circular(40))
+                          ),
+                          child: const Icon(
+                            Icons.arrow_forward,
+                            color: kBackgroundColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],
